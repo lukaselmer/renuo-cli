772e883e69266eb69ef21378373851b60a8e57f4
Lukas Elmer <lukas.elmer@gmail.com>
2015-11-05 18:17:28 +0100

    local storage, prepare name display

---
+++ app/local_storage.rb
@@ -0,0 +1,30 @@
+require 'json'
+
+class LocalStorage
+  def store(key, value)
+    setup
+    config = load_config
+    config[key.to_s] = value
+    write_config(config)
+  end
+
+  def load(key)
+    config = load_config
+    config[key.to_s]
+  end
+
+  private
+  def load_config
+    JSON.parse(File.read('.local_storage'))
+  end
+
+  def write_config(config)
+    File.write('.local_storage', config.to_json)
+    File.chmod(0600, '.local_storage')
+  end
+
+  def setup
+    write_config({}) unless File.exists? '.local_storage'
+  end
+end
+
