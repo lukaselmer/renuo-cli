cb7d08deb43247114121fc5a09cdbab47d52a5a5
Lukas Elmer <lukas.elmer@gmail.com>
2015-11-08 23:23:32 +0100

    refactoring

--- lib/renuo/cli.rb
+++ lib/renuo/cli.rb
@@ -1,32 +1,36 @@
 require 'renuo/cli/version'
 require 'rubygems'
-require 'commander/import'
 require 'renuo/cli/app/name_display'
 require 'renuo/cli/app/local_storage'
 
-program :version, '0.0.1'
-program :description, 'renuo CLI'
+class Renuo::CLI
+  def self.start
+    require 'commander/import'
+    program :version, '0.0.1'
+    program :description, 'renuo CLI'
 
-command 'display-name'.to_sym do |c|
-  c.syntax = 'renuo display-name [options]'
-  c.summary = 'displays the name of a customer'
-  c.description = ''
-  c.example 'description', 'command example'
-  c.option '--delete', 'Deletes the current name'
-  c.action do |args, _options|
-    NameDisplay.new.display_name(args)
-  end
-end
+    command 'display-name'.to_sym do |c|
+      c.syntax = 'renuo display-name [options]'
+      c.summary = 'displays the name of a customer'
+      c.description = ''
+      c.example 'description', 'command example'
+      c.option '--delete', 'Deletes the current name'
+      c.action do |args, _options|
+        NameDisplay.new.display_name(args)
+      end
+    end
 
-command :config do |c|
-  c.syntax = 'renuo config [options]'
-  c.summary = ''
-  c.description = ''
-  c.example 'description', 'command example'
-  c.option '--some-switch', 'Some switch that does something'
-  c.action do |_args, _options|
-    key = ask('API Key?') { |q| q.echo = '*' }
-    LocalStorage.new.store(:api_key, key)
-    say('stored the api key')
+    command :config do |c|
+      c.syntax = 'renuo config [options]'
+      c.summary = ''
+      c.description = ''
+      c.example 'description', 'command example'
+      c.option '--some-switch', 'Some switch that does something'
+      c.action do |_args, _options|
+        key = ask('API Key?') { |q| q.echo = '*' }
+        LocalStorage.new.store(:api_key, key)
+        say('stored the api key')
+      end
+    end
   end
 end
