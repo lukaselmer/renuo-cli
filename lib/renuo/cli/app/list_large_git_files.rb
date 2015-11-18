01fa1cce315e7e72786cac93c6c9cce989325c85
Lukas Elmer <lukas.elmer@gmail.com>
2015-11-18 14:09:13 +0100

    list large git files

---
+++ lib/renuo/cli/app/list_large_git_files.rb
@@ -0,0 +1,7 @@
+class ListLargeGitFiles
+  def run
+    cmd = 'join -o "1.1 1.2 2.3" <(git rev-list --objects --all | sort) '\
+    '<(git verify-pack -v objects/pack/*.idx | sort -k3 -n | tail -5 | sort) | sort -k3 -n`'
+    puts `#{cmd}`
+  end
+end
