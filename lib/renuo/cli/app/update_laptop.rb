127c6c1db2020f4c0b838153f75f8c43f802b9ef
Pascal Andermatt <pascal.andermatt@renuo.ch>
2016-09-09 16:09:53 +0200

    UpdateLaptop + Added update_laptop.rb 	+ say_hi 	+ brewupdate 	+ brewupgrade 	+ brewcleanup 	+ mac_update

---
+++ lib/renuo/cli/app/update_laptop.rb
@@ -0,0 +1,43 @@
+class UpdateLaptop
+  def run
+    say_hi
+    brewupdate
+    brewupgrade
+    brewcleanup
+    mac_update
+  end
+
+  def brewupdate
+    say 'brew update'
+    say ''
+    puts `$brew update`
+    say '-------------------------------'
+  end
+
+  def brewupgrade
+    say 'brew upgrade'
+    say ''
+    puts `$brew upgrade`
+    say '-------------------------------'
+  end
+
+  def brewcleanup
+    say 'brew cleanup'
+    say ''
+    puts `$brew cleanup`
+    say '-------------------------------'
+  end
+
+  def mac_update
+    say ' MacBook Pro Update'
+    say ''
+    puts `$sudo softwareupdate -iva`
+    say '-------------------------------'
+  end
+
+  def say_hi
+    say 'Updating PC'
+    say 'Start Update'
+    say '_______________________________'
+  end
+end
