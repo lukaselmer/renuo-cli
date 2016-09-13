a6a7be49886c9700fc5cbdc5f6ed275d03360e9f
Pascal Andermatt <pascal.andermatt@renuo.ch>
2016-09-13 14:45:25 +0200

    Bug Fixes and Improvements + Ask for Reboot + Colorized Output + Added Countdown

--- lib/renuo/cli/app/update_laptop.rb
+++ lib/renuo/cli/app/update_laptop.rb
@@ -8,24 +8,43 @@ class UpdateLaptop
   end
 
   def say_hi
-    say 'Updating PC'
-    say 'Start Update'
-    say '_______________________________'
+    say 'Updating PC'.brown
+    say 'Start Update'.brown
+    say '_______________________________'.bold
   end
 
   def run_cmd(name)
-    say name.to_s
+    say name.to_s.brown
     say ''
     puts `#{name}`
-    say '-------------------------------'
+    say '_______________________________'.bold
   end
 
   def mac_update
-    say ' MacBook Pro Update'
+    say ' MacBook Pro Update'.brown
     say ''
-    puts `sudo softwareupdate -iva`
-    say '-------------------------------'
-    agree('Rebooting Now?')
-    puts `sudo shutdown -r now "Rebooting Now"`
+    puts `sudo softwareupdate -ia`
+    say '_______________________________'.bold
+    if agree('Reboot Now? (Yes/No)'.red)
+      countdown(5)
+      puts `sudo shutdown -r now "Rebooting Now"`.gray.bg_red
+    end
   end
+
+    def countdown(n)
+      for i in (n).downto(1)
+      say "Reboot in #{i} Seconds (Press Ctrl + C to Stop)".red
+      say `sleep 1`
+    end
+  end
+
+end
+
+
+#Colorized Output
+class String
+def red;            "\e[31m#{self}\e[0m" end
+def gray;           "\e[37m#{self}\e[0m" end
+def bg_red;         "\e[41m#{self}\e[0m" end
+def bold;           "\e[1m#{self}\e[22m" end
 end
