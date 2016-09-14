61d1c67a9810399a72c4dd6467bc21be5419970b
Pascal Andermatt <pascal.andermatt@renuo.ch>
2016-09-14 15:29:27 +0200

    Added upgrade_mac_os.rb

--- lib/renuo/cli/app/update_laptop.rb
+++ lib/renuo/cli/app/update_laptop.rb
@@ -1,4 +1,7 @@
+require_relative 'upgrade_mac_os.rb'
 class UpdateLaptop
+  include UpgradeMacOS
+
   def run
     setup_mas
 
@@ -21,47 +24,9 @@ class UpdateLaptop
     run_command 'mas upgrade'
   end
 
-  def upgrade_mac_os
-    say 'Updating  macOS (this may take a while)'.yellow
-
-    output = `softwareupdate --list 2>&1`
-    puts output
-
-    return unless update_available output
-
-    if reboot_required output
-      return unless ask_for_update
-    end
-
-    run_command 'softwareupdate --install --all'
-    reboot if reboot_required output
-  end
-
-  def reboot_required(output)
-    output.include? '[restart]'
-  end
-
-  def update_available(output)
-    !(output.include? 'No new software available')
-  end
-
-  def ask_for_update
-    agree('Your Mac needs to be rebooted, Still continue? (Yes / No)')
-  end
-
-  def reboot
-    say 'Rebooting in 60 Seconds'.white.on_red
-    puts `osascript -e 'tell app "loginwindow" to «event aevtrrst»'`
-  end
-
   def upgrade_brew
     run_command 'brew update'
     run_command 'brew upgrade'
     run_command 'brew cleanup'
-  end
-
-  def run_command(command)
-    say command.to_s.yellow
-    system command
   end
 end
---
+++ lib/renuo/cli/app/upgrade_mac_os.rb
@@ -0,0 +1,39 @@
+module UpgradeMacOS
+  def upgrade_mac_os
+    say 'Updating  macOS (this may take a while)'.yellow
+
+    output = `softwareupdate --list 2>&1`
+    puts output
+
+    return unless update_available output
+
+    if reboot_required output
+      return unless ask_for_update_and_reboot
+    end
+
+    run_command 'softwareupdate --install --all'
+    reboot if reboot_required output
+  end
+
+  def update_available(output)
+    !(output.include? 'No new software available')
+  end
+
+  def reboot_required(output)
+    output.include? '[restart]'
+  end
+
+  def ask_for_update_and_reboot
+    agree('Your Mac needs to be rebooted, Still continue? (Yes / No)')
+  end
+
+  def reboot
+    say 'Rebooting in 60 Seconds'.white.on_red
+    puts `osascript -e 'tell app "loginwindow" to «event aevtrrst»'`
+  end
+
+  def run_command(command)
+    say command.to_s.yellow
+    system command
+  end
+end
